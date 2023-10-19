3434b14106f74f0ae512f744c2fb5e60f4c10015
Ahmed AlShaiba <ahmde031@gmail.com>
2023-10-19 09:45:21 +0400

    install+config tailwindcss, create+styled Home page

---
+++ config/tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    colors:{
+      primary: "#222831",
+      secondary: "#00ADB5",
+      slight: "#00ADB5",
+      plight: "#EEEEEE"
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
